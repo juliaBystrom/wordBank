b21c29fde8a561961d70bbdf2f069cb5b9ddff7b
Christofferos <kristopher.werlinder@telia.com>
2021-03-16 13:35:43 +0100

    Firebase config stuff

---
+++ public/firebaseConfig.js
@@ -0,0 +1,11 @@
+/* // Your web app's Firebase configuration
+let firebaseConfig = {
+  apiKey: "AIzaSyCRpMoP4rszSQuEszRczsGFTkZ79y2Pikg",
+  authDomain: "wordbank-99cc5.firebaseapp.com",
+  projectId: "wordbank-99cc5",
+  storageBucket: "wordbank-99cc5.appspot.com",
+  messagingSenderId: "206331525710",
+  appId: "1:206331525710:web:cd890d231a4e575745611f",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig); */
