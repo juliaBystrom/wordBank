40910c84494d2db1d9b1255e04a22547bc895ee7
Julia Byström <juliavasastan@gmail.com>
2021-04-06 14:57:36 +0200

    Move styled commponets for translation card into its own file

--- src/views/cardView.js
+++ src/views/cardView.js
@@ -1,33 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 
+
 /*
-Usage notes:
-    The Card component gives the style of the component. It can be combinated with the Translation component
-    however if span is the other component then the margin area of Translation will also be preasable. So dont do that. 
+    CardView
+    Used in boards
+
 */
 
-const Card = styled.div`
-    margin: 2px;
-    padding: 2px;
-`;
-
-const TranslationWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-around;
-`;
-
-const Translation = styled.div`
-    background-color: ${props => (props.isTranslateFrom ? props.theme.fromLanguage : props.theme.toLanguage)};
-    padding: 10px;
-    color: ${props => props.theme.darkText};
-    text-align: center;
-    font-size: 1em;
-    // flex: 0 0 50px;
-    width: 50%;
-    border-radius: ${props => (props.isTranslateFrom ? "5px 0px 0px 5px" : "0px 5px 5px 0px" )}; /* top left, top right, bottom right, bottom left */
-`;
+import { Card, TranslationWrapper, Translation } from "./components"
 
 export default function CardView(props) {
     return (
