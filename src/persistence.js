6a9863d7028af4e66772e8aca3ce530a89a672d6
Fanny Voltaire <fannyev@kth.se>
2021-04-12 20:46:46 +0200

    draft of class-conversion of model to firestore

--- src/persistence.js
+++ src/persistence.js
@@ -7,6 +7,7 @@ import { format } from "prettier";
  * 
  */
 
+
 export function persistence(model) {
     let loadingFromFirebase = false;
     let cardNum = 0;
@@ -15,12 +16,76 @@ export function persistence(model) {
         setTimeout(() => {
           window.db
           .collection("Users").doc(model.userID)
-          .collection("Banks").doc(model.banks[0])
-          .set({ //hårdkodat, måste ändras sen
-            phrase: model.currentPhrase,
-            translation: model.currentTranslation,
+          .set({
+            userID: model.userID,
+            name: "Fanny"
           });
+          
+          window.db
+          .collection("Users").doc(model.userID)
+          .collection("Banks").doc(model.currentBank)
+          .set({
+            bankID: model.bankID,
+            name: "Fanny"
+          });
+
+          ;
         }, 1000);
       }
     cardNum++;});
   }
+
+// Firestore data converter
+var modelConverter = {
+  toFirestore: function(model) {
+      return {
+          currentBank: model.currentBank,
+          observers: model.observers,
+          userID: model.userID,
+          banks: model.banks,
+          languageFrom: model.languageFrom,
+          languageTo: model.languageTo,
+          sorts: model.sorts
+          };
+  },
+  fromFirestore: function(snapshot, options){
+      const data = snapshot.data(options);
+      return new Model
+      (
+        data.currentBank,
+        data.observers,
+        data.userID,
+        data.banks,
+        data.languageFrom,
+        data.languageTo,
+        data.sorts
+      );
+  }
+};
+
+// Set with modelConverter
+db.collection("models").doc("user")
+  .withConverter(modelConverter)
+  .set(new Model("..."));
+
+
+// class User {
+//   constructor (userID, name) {
+//     this.userID = userID;  
+//     this.name = name;
+//   }
+// }
+
+// // Firestore data converter
+// var userConverter = {
+//   toFirestore: function(user) {
+//       return {
+//           userID: user.userID,
+//           name: user.name,
+//           };
+//   },
+//   fromFirestore: function(snapshot, options){
+//       const data = snapshot.data(options);
+//       return new User(user.userID, user.name);
+//   }
+// };
