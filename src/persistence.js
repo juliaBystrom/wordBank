9ff83bf8969c34376e1b3100d84e23b91e6cf047
Fanny Voltaire <fannyev@kth.se>
2021-04-14 11:00:15 +0200

    adds user -> banks succesfully

--- src/persistence.js
+++ src/persistence.js
@@ -6,32 +6,6 @@ import {WordBankModel} from "./models/wordBankModel";
  */
 
  export function persistence(model) {
-  // Firestore data converter
-  var modelConverter = {
-    toFirestore: function(model) {
-        return {
-            currentBank: model.currentBank,
-            userID: model.userID,
-            languageFrom: model.languageFrom,
-            languageTo: model.languageTo,
-            banks: model.banks.map(bank => { 
-              var id = bank.bankID; 
-              return {id: bank}
-            })
-            };
-
-    },
-    fromFirestore: function(snapshot, options){
-        const data = snapshot.data(options);
-        return new WordBankModel
-        (
-          data.currentBank,
-          data.userID,
-          data.languageFrom,
-          data.languageTo,
-        );
-    }
-  };
 
     let loadingFromFirebase = false;
     let cardNum = 0;
@@ -39,53 +13,20 @@ import {WordBankModel} from "./models/wordBankModel";
       if (!loadingFromFirebase) {
         setTimeout(() => {
           window.db
-            .collection("models").doc("123")
-            .withConverter(modelConverter)
-            .set(model)
-            /* .then(() => {
-              model.banks.forEach((bank) => {
-                window.db
-                .collection("models").doc("123")
-                .collection("banks").doc(String(bank.bankID))
-                .set({
+            .collection("users").doc(String(model.userID))
+            .set({activeBankID: String(model.currentBank)})
+            .then(model.banks.forEach(bank=>{
+              window.db
+              .collection("users").doc(String(model.userID))
+              .collection("banks").doc(String(bank.bankID))
+              .set({
                   languageFrom: bank.languageFrom,
                   languageTo: bank.languageTo,
-                  });
-                  
-                  bank.boards.forEach((board) =>{
-                    window.db
-                    .collection("models").doc("123")
-                    .collection("banks").doc(String(bank.bankID))
-                    .collection("boards").doc(board.boardID)
-                    .set({
-                      title: board.title
-                      })
-                  })
-              }) 
-              }) */
+                  tags: bank.tags.map((tag)=> {return String(tag.tag)})
+              })
+            }))
         }, 1000);
       }
-    });    
+    }); 
+
   }
-
-
-// class User {
-//   constructor (userID, name) {
-//     this.userID = userID;  
-//     this.name = name;
-//   }
-// }
-
-// // Firestore data converter
-// var userConverter = {
-//   toFirestore: function(user) {
-//       return {
-//           userID: user.userID,
-//           name: user.name,
-//           };
-//   },
-//   fromFirestore: function(snapshot, options){
-//       const data = snapshot.data(options);
-//       return new User(user.userID, user.name);
-//   }
-// };
